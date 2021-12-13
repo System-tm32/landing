b4bcabd6223246bcf69afb8647362df730b21aa6
Kirill Ilin <ilin@uchi.ru>
2021-12-13 16:48:53 +0300

    feat: styling

--- src/components/Popup/index.tsx
+++ src/components/Popup/index.tsx
@@ -1,8 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
 import { PopupProps } from './type';
 
-const Popup = ({ children }: PopupProps): JSX.Element => {
-  return <>{children}</>;
+const Popup = ({ children, onClick }: PopupProps): JSX.Element => {
+  return (
+    <Overlay onClick={onClick}>
+      <Wrapper>{children}</Wrapper>
+    </Overlay>
+  );
 };
 
 export { Popup };
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-content: space-between;
+  max-width: 640px;
+  background: #fff;
+  padding: 24px;
+  border-radius: 16px;
+`;
+
+const Overlay = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+`;
