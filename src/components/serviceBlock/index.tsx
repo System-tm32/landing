0024377e8904b86dd6d618d9249c2e259ce3fe11
Kirill Ilin <ilin@uchi.ru>
2021-12-15 16:26:35 +0300

    chore: init files

---
+++ src/components/serviceBlock/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ServiceBlockProps } from './type';
+
+const ServiceBLock = ({ label, description }: ServiceBlockProps) => {
+  return <></>;
+};
+
+export { ServiceBLock };
---
+++ src/components/serviceBlock/type.ts
@@ -0,0 +1,4 @@
+export interface ServiceBlockProps {
+  label: string;
+  description: string;
+}
