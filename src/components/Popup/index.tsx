b7c770e7d8551bab4a1f98eae94d1481b15afb2a
Kirill Ilin <ilin@uchi.ru>
2021-12-15 16:03:01 +0300

    feat: add custom.d.ts for svg files, add button close

--- src/components/Popup/index.tsx
+++ src/components/Popup/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
+import closeSprite from './img/close.svg';
 import { PopupProps } from './type';
 
 const Popup = ({ children, onClick }: PopupProps): JSX.Element => {
   return (
     <Overlay onClick={onClick}>
-      <Wrapper>{children}</Wrapper>
+      <Wrapper>
+        <CloseButton onClick={onClick}>
+          <img src={closeSprite} />
+        </CloseButton>
+        {children}
+      </Wrapper>
     </Overlay>
   );
 };
@@ -13,6 +19,7 @@ const Popup = ({ children, onClick }: PopupProps): JSX.Element => {
 export { Popup };
 
 const Wrapper = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
   align-content: space-between;
@@ -30,4 +37,24 @@ const Overlay = styled.div`
   width: 100%;
   height: 100%;
   background: rgba(50, 48, 63, 0.5);
+`;
+
+const CloseButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  position: absolute;
+  right: 24px;
+  top: 24px;
+  width: 46px;
+  height: 46px;
+  border-radius: 50%;
+  border: 1px solid #e53217;
+  cursor: pointer;
+  background: none;
+
+  &:hover {
+    background: #fbe0dc;
+  }
 `;
